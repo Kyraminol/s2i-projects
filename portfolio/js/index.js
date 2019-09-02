ac6a0401166f7557f4e2ea16cc807eebd2b20879
Kyraminol <kyraminari@gmail.com>
2019-09-02 18:03:18 +0200

    Aggiunto dropdown lingua, sistemati errori di layout e aggiunto init delle navbar ad ogni ridimensionamento pagina

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -1,15 +1,23 @@
 $(document).ready(function(){
-  $('nav').each(function() {
-    var $this = $(this);
-    var $target = $('#' + $(this).attr('data-target'));
-    M.Pushpin.init($this.get(0), {
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  var initNav = function(){
+    $('nav').each(function(){
+      var $this = $(this);
+      var $target = $('#' + $(this).attr('data-target'));
+      M.Pushpin.init($this.get(0), {
+        top: $target.offset().top,
+        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+      });
     });
+  };
+  $(window).resize(function(){
+    initNav();
   });
 
+  initNav();
   M.Parallax.init(document.querySelectorAll('.parallax'));
   M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {hover: true});
   M.Carousel.init(document.querySelectorAll('.carousel'), {fullWidth: true, indicators: true});
+
+
 
 });
