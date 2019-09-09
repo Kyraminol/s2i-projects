3a45af7ab321475601cc9a4c8a4744e807c0bef8
Kyraminol <kyraminari@gmail.com>
2019-09-09 20:02:30 +0200

    [portfolio] Aggiunta chiusura dropdown navbar al cambio di navbar (per pushpin)

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -9,19 +9,30 @@ $(document).ready(function(){
       var $target = $('#' + $(this).attr('data-target'));
       M.Pushpin.init($this.get(0), {
         top: $target.offset().top,
-        bottom: $target.offset().top + $target.outerHeight() - $this.height()
+        bottom: $target.offset().top + $target.outerHeight() - $this.height(),
+        onPositionChange: onPushpinChange,
       });
     });
   };
+
+  var parallaxes = M.Parallax.init(document.querySelectorAll('.parallax'));
+  var dropdowns = M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {hover: false, constrainWidth: false, coverTrigger: false});
+  var carousels = M.Carousel.init(document.querySelectorAll('.carousel'), {fullWidth: true, indicators: true});
+
+  var onPushpinChange = function(){
+    var el = $(this.el);
+    if(el.hasClass('pin-top') || el.hasClass('pin-bottom')){
+      dropdowns.forEach(function(dropdown){
+        if(dropdown.id.startsWith('nav-')){
+          dropdown.close();
+        }
+      });
+    }
+  };
+
   $(window).resize(function(){
     initNav();
   });
-
   initNav();
-  M.Parallax.init(document.querySelectorAll('.parallax'));
-  M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {hover: false, constrainWidth: false, coverTrigger: false});
-  M.Carousel.init(document.querySelectorAll('.carousel'), {fullWidth: true, indicators: true});
-
-
 
 });
