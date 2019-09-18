3b0683a0d153481517cb5233bf4eb62ad20c56fd
Kyraminol <kyraminari@gmail.com>
2019-09-18 18:52:44 +0200

    [portfolio] Sistema di temi completato

--- portfolio/js/theme.js
+++ portfolio/js/theme.js
@@ -1,73 +1,94 @@
 $(function(){
   const main_colors = ['red', 'pink', 'purple', 'deep-purple', 'indigo', 'blue', 'light-blue', 'cyan', 'teal', 'green','light-green', 'lime', 'yellow', 'amber', 'orange', 'deep-orange', 'brown', 'grey', 'blue-grey', 'white', 'black'];
-  const nuances = ['darken-4', 'darken-3', 'darken-2', 'darken-1', '', 'lighten-1', 'lighten-2', 'lighten-3', 'lighten-4', 'lighten-5', 'accent-1', 'accent-2', 'accent-3', 'accent-4'];
+  const nuances = ['darken-4', 'darken-3', 'darken-2', 'darken-1', 'default-nuance', 'lighten-1', 'lighten-2', 'lighten-3', 'lighten-4', 'lighten-5', 'accent-1', 'accent-2', 'accent-3', 'accent-4'];
   const background_colors = ['white', 'grey', 'blue-grey', 'black'];
 
-  let initPicker = function(selector, default_main, default_background){
+  const appendCircles = function(element, list){
+    list.forEach(function(item){
+      element.append('<div class="circle-color" data-theme-class="' + item + '"><a href="#"><div class="' + item + '"></div></a></div>');
+    });
+  };
+
+  const initPicker = function(selector){
     if(selector === undefined){
       selector = '.theme-picker';
     }
-    if(default_main === undefined){
-      default_main = 'blue';
-    }
-    if(default_background === undefined){
-      default_background = 'white';
-    }
+
     $(selector).each(function(){
       let $this = $(this);
-      let $main_colors = $('<div></div>');
-      let $nuances = $('<div></div>');
-      let $background_colors = $('<div></div>');
+      let target = $this.data('theme-target');
+      let default_main = $this.data('theme-main');
+      let default_text = $this.data('theme-text');
+      let default_background = $this.data('theme-background');
 
-      main_colors.forEach(function(item){
-        let parent_class = '';
-        if(item === default_main){
-          parent_class = ' selected default';
-        }
-        $main_colors.append('<div class="circle-color' + parent_class + '"><a href="#"><div class="' + item + '"></div></a></div>');
-      });
-
-      nuances.forEach(function(item){
-        let parent_class = '';
-        if(item === ''){
-          parent_class = ' selected default';
-        }
-        $nuances.append('<div class="circle-color' + parent_class + '"><a href="#"><div class="' + default_main + ' ' + item + '"></div></a></div>');
-      });
-
-      background_colors.forEach(function(item){
-        let parent_class = '';
-        if(item === default_background){
-          parent_class = ' selected default';
-        }
-        $background_colors.append('<div class="circle-color' + parent_class + '"><a href="#"><div class="' + item + '"></div></a></div>');
-      });
+      let $main_colors = $('<div class="theme-main theme-' + target + '"></div>');
+      let $nuances = $('<div class="theme-nuances theme-' + target + '"></div>');
+      let $text_colors = $('<div class="theme-text theme-' + target + '"></div>');
+      let $background_colors = $('<div class="theme-background theme-' + target + '"></div>');
+      appendCircles($main_colors, main_colors);
+      appendCircles($nuances, nuances);
+      appendCircles($text_colors, main_colors);
+      appendCircles($background_colors, background_colors);
 
       $this.append('<h4 class="i18n-main-color">Main Color</h4>');
       $this.append($main_colors);
-      $this.append('<h4 class="i18n-nuance">Nuance</h4>');
+      $this.append('<h5 class="i18n-nuance">Nuance</h5>');
       $this.append($nuances);
-      $this.append('<h4 class="i18n-nuance">Background Color</h4>');
+      $this.append('<h4 class="i18n-text-color">Text Color</h4>');
+      $this.append($text_colors);
+      $this.append('<h4 class="i18n-background-color">Background Color</h4>');
       $this.append($background_colors);
+
+      $('.theme-main.theme-' + target + ' > div:has(div.' + default_main + ')').addClass('selected');
+      $('.theme-text.theme-' + target + ' > div:has(div.' + default_text + ')').addClass('selected');
+      $('.theme-background.theme-' + target + ' > div:has(div.' + default_background + ')').addClass('selected');
+      $('.theme-nuances.theme-' + target + ' a > div').addClass(default_main);
+      $('.theme-nuances.theme-' + target + ' .circle-color').addClass('small');
 
     });
   };
 
   initPicker();
 
-  const theme_apply = function(old_color, new_color, section){
-    $('.theme-' + section).removeClass(old_color).addClass(new_color);
+  const theme_apply = function(selector ,old_class, new_class){
+    $(selector).removeClass(old_class).addClass(new_class);
   };
 
   $('div.circle-color').on('click', function(e){
     e.preventDefault();
     let $this = $(this);
+    let $parent = $this.parent();
     let $old = $this.siblings('.selected');
-    let old_color = $old.find('a > div').get(0).classList[0];
-    let new_color = $this.find('a > div').get(0).classList[0];
+    let new_class = $this.data('theme-class');
+    let old_class = '';
+    if($old.length > 0){
+      old_class = $old.data('theme-class');
+    }
+    let target = $this.parents('.theme-picker').first().data('theme-target');
     $old.removeClass('selected');
     $this.addClass('selected');
-    theme_apply(old_color, new_color, 'aboutme-main');
+
+    if($parent.hasClass('theme-main')){
+      theme_apply('.theme-nuances.theme-' + target + ' a > div', old_class, new_class);
+      $('nav.theme-' + target).data('fab', new_class);
+      $('#fab').removeClass(old_class).addClass(new_class);
+      target += '-main';
+    } else if ($parent.hasClass('theme-nuances')){
+      target += '-main';
+    } else if($parent.hasClass('theme-background')){
+      target += '-background';
+      if(new_class === 'white'){
+        theme_apply('.theme-' + target + '-text', 'white-text', 'black-text');
+      } else {
+        theme_apply( '.theme-' + target + '-text', 'black-text', 'white-text');
+      }
+    } else if($parent.hasClass('theme-text')){
+      old_class += '-text';
+      new_class += '-text';
+      target += '-text';
+    }
+
+    theme_apply( '.theme-' + target, old_class, new_class);
   });
 
 });
