8a42cf8047f06d1fe26ba9fbfbf4db3fc91f8d52
Kyraminol <kyraminari@gmail.com>
2019-10-31 18:23:28 +0100

    [weatherize] Aggiunti commenti

--- weatherize/js/weatherize.js
+++ weatherize/js/weatherize.js
@@ -1,13 +1,17 @@
 function Weatherize(apiKey){
+  // Check if apiKey is passed as an argument and it is a string
   if(apiKey === undefined || typeof apiKey !== 'string'){
     console.error('apiKey must be provided as string.');
   }
   this._apiKey = apiKey;
 
+  // Define getWeather function
   this.getWeather = function(city){
+    // Check if city is passed as an argument and it is a string
     if(city === undefined || typeof city !== 'string'){
       return null;
     }
+    // Make the ajax call
     $.ajax({
       url: 'https://api.openweathermap.org/data/2.5/forecast',
       data: {
@@ -15,6 +19,7 @@ function Weatherize(apiKey){
         appid: apiKey,
       },
     }).then(r => {
+      // Parse the response
       console.log(r);
     });
   }
