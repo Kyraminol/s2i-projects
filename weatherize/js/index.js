b06fc0b9106c0323c0a150d96dba4e37cefcef26
Kyraminol <kyraminari@gmail.com>
2019-11-04 13:09:15 +0100

    [weatherize] Aggiunti stili per il selettore dei giorni, aggiunto riempimento automatico delle classi weather-date, weather-week-day, weather-icon, weather-temp, weather-temp-max, weather-temp-min Aggiunta classe weather-dN dove N sta per il giorno corrispondente per il riempimento automatico, aggiunti dropdown lingua e impostazioni nella navbar Aggiunto input per chiave API

--- weatherize/js/index.js
+++ weatherize/js/index.js
@@ -2,14 +2,29 @@ $(function(){
 
   // callback function to do additional stuff
   let callback = function(data){
+    $('#day-picker-row').removeClass('hide');
     console.log(data);
   };
 
+  M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'), {hover: false, constrainWidth: false, coverTrigger: false, closeOnClick: false});
+
   // instantiate Weatherize object
-  let weatherize = new Weatherize('API-KEY-HERE');
+  let weatherize = undefined;
 
-  // call main function
-  weatherize.getWeather('London', callback);
+  $('#day-picker > a').on('click', function(e){
+    e.preventDefault();
+    let $this = $(this);
+    if($this.hasClass('selected')){
+      return;
+    }
+    $this.siblings('.selected').removeClass('selected');
+    $this.addClass('selected');
 
+  });
+
+  $('#api-key-accept').on('click', function(e){
+    e.preventDefault();
+    weatherize = new Weatherize($('#api-key').val());
+  });
 
 });
--- weatherize/js/weatherize.js
+++ weatherize/js/weatherize.js
@@ -7,6 +7,12 @@ function Weatherize(apiKey){
 
   // Define getWeather function
   this.getWeather = function(city, callback){
+
+    let round = function round(value, precision) {
+      let multiplier = Math.pow(10, precision || 0);
+      return Math.round(value * multiplier) / multiplier;
+    };
+
     // Check if city is passed as an argument and it is a string
     if(city === undefined || typeof city !== 'string'){
       return null;
@@ -17,16 +23,36 @@ function Weatherize(apiKey){
       data: {
         q: city,
         appid: apiKey,
+        units: 'metric',
       },
     }).then(r => {
-      // Parse the response
+      // Parse the response in a date ordered object
       console.log(r);
       let result = {};
       r.list.forEach(function(weather){
-        let date = new Date(weather['dt'] * 1000);
-        let datestring = date.getDate() + '/' + date.getMonth() +  '/' + date.getFullYear();
-        result[datestring] = weather;
+        let datestr = weather['dt_txt'].split(' ');
+
+        if(!Object.keys(result).includes(datestr[0])){
+          result[datestr[0]] = [];
+        }
+        result[datestr[0]].push(weather);
       });
+
+      let resultKeys = Object.keys(result);
+
+      for(let i = 0; i < resultKeys.length; i++){
+        let weather = result[resultKeys[i]];
+        let date = new Date(resultKeys[i]);
+        let $d = $('.weather-d' + (i + 1));
+        $d.filter('.weather-week-day').text(date.toLocaleDateString('it-IT', {weekday: 'short'}));
+        $d.filter('.weather-date').text(date.toLocaleDateString('it-IT', {day: '2-digit', month: '2-digit'}));
+        $d.filter('.weather-icon').attr('src', 'http://openweathermap.org/img/wn/' + weather[0]['weather'][0]['icon'] + '.png');
+        $d.filter('.weather-temp').text(round(weather[0]['main']['temp'], 1) + '°C');
+        $d.filter('.weather-temp-max').text(round(weather[0]['main']['temp_max'], 1) + '°C');
+        $d.filter('.weather-temp-min').text(round(weather[0]['main']['temp_min'], 1) + '°C');
+      }
+
+
 
       // Check if callback is passed as a function, then call it
       if(typeof callback === 'function'){
