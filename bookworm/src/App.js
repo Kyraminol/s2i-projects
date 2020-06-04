8c1a8f28a94662d61587b7545f8609e4050ca63d
Kyraminol <kyraminari@gmail.com>
2020-06-05 00:12:32 +0200

    [bookworm] Aggiunto tema, bottone login in navbar e campo di ricerca

--- bookworm/src/App.js
+++ bookworm/src/App.js
@@ -4,19 +4,69 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { makeStyles } from '@material-ui/core/styles';
 import './App.css';
 
 function App() {
-  const classes = useStyles();
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+    [prefersDarkMode],
+  );
+
+  const classes = makeStyles((theme) => ({
+    '@global': {
+      ul: {
+        margin: 0,
+        padding: 0,
+        listStyle: 'none',
+      },
+    },
+    appBar: {
+      borderBottom: `1px solid ${theme.palette.divider}`,
+    },
+    toolbar: {
+      flexWrap: 'wrap',
+    },
+    toolbarTitle: {
+      flexGrow: 1,
+    },
+    icon: {
+      marginRight: theme.spacing(2),
+    },
+    landing: {
+      backgroundColor: theme.palette.background.paper,
+      paddingTop: theme.spacing(4),
+      paddingBottom: theme.spacing(4),
+    },
+    footer: {
+      backgroundColor: theme.palette.background.paper,
+      padding: theme.spacing(6),
+    },
+  }), {defaultTheme: theme})();
 
   return (
-      <React.Fragment>
+    <React.Fragment>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography variant="h5" color="inherit" noWrap> Bookworm </Typography>
+        <AppBar position="static" color="primary" elevation={0} className={classes.appBar}>
+          <Toolbar className={classes.toolbar}>
+            <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
+              Bookworm
+            </Typography>
+            <Button href="#" color="inherit" variant="outlined" className={classes.link}>
+              Login with Google
+            </Button>
           </Toolbar>
         </AppBar>
         <main>
@@ -27,11 +77,15 @@ function App() {
               </Typography>
               <Typography variant="h5" align="center" color="textSecondary" paragraph>
                 Your personal cozy companion for searching and wishlisting books.
-                </Typography>
+              </Typography>
               <Typography align="center" color="textSecondary" paragraph>
                 I can remember which books you already completed and your progress at reading!
               </Typography>
-
+            </Container>
+            <Container maxWidth="md">
+              <form className={classes.root} noValidate autoComplete="off">
+                <TextField id="search-input" placeholder="Search for books you love" variant="outlined" fullWidth margin="normal" InputLabelProps={{shrink: true,}}/>
+              </form>
             </Container>
           </div>
         </main>
@@ -44,31 +98,17 @@ function App() {
           </Typography>
           <Copyright/>
         </footer>
-      </React.Fragment>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  landing: {
-    backgroundColor: theme.palette.background.paper,
-    paddingTop: theme.spacing(4),
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
-
 function Copyright() {
   return (
-      <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © Gianmarco Randazzo '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © Gianmarco Randazzo '}
+      {new Date().getFullYear()}
+    </Typography>
   );
 }
 
